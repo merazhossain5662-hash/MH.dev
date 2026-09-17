17736698dafcf80a27d9e634eb951c1b37e0f2c6
boboiboy <merazhossain5662@gmail.com>
2026-09-17 18:31:01 +0600

    added deployed server url for timeline

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -68,7 +68,7 @@ export default function Timeline() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch("http://localhost:5000/api/timeline")
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}api/timeline`)
       .then((res) => {
         if (!res.ok) throw new Error("Failed to fetch");
         return res.json();
